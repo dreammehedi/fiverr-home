63f4d94ee385d7a9160009af2c2482a2601566d4
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-02-25 20:44:05 +0600

    Menu scroll to Sticky

--- main.js
+++ main.js
@@ -5,3 +5,27 @@ toggleBtn.addEventListener("click", () => {
   mobileMenu.classList.toggle("opacity-100");
   mobileMenu.classList.toggle("-left-0");
 });
+
+// scroll to sticky menu
+window.addEventListener("scroll", () => {
+  const navbar = document.getElementById("nav");
+  if (window.scrollY > 150) {
+    navbar.classList.add(
+      "!bg-black/80",
+      "fixed",
+      "w-full",
+      "h-auto",
+      "top-0",
+      "left-0"
+    );
+  } else {
+    navbar.classList.remove(
+      "!bg-black/80",
+      "fixed",
+      "w-full",
+      "h-auto",
+      "top-0",
+      "left-0"
+    );
+  }
+});
