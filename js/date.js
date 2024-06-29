808d6c4697d374195b1c63e62a2a73f52b3470ca
dreammehedi <dreammehedihassan@gmail.com>
2024-06-29 08:47:25 +0600

    Home Page Design Succes

--- js/date.js
+++
@@ -1,4 +0,0 @@
-const footerYear = document.getElementById("footer-year");
-const date = new Date();
-const year = date.getFullYear();
-footerYear.innerText = year;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,30 +2,22 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1400px",
+    },
     fontFamily: {
-      barlow: ["Barlow", "sans-serif"],
-      "chakra-petch": ["Chakra Petch", "sans-serif"],
+      inter: ["Inter", "sans-serif"],
     },
     container: {
       center: true,
       padding: {
-        DEFAULT: "1.5rem",
+        DEFAULT: "1rem",
       },
     },
-    extend: {
-      colors: {
-        "neutral-100": "white",
-        "neutral-200": "#dfe2e5",
-        "primary-1": "#f32c46",
-        "neutral-600": "#282d34",
-        "secondary-1": "#202329",
-        "neutral-700": "#11161e",
-        "neutral-500": "#555b63",
-        "neutral-400": "#82878c",
-        "neutral-300": "#abafb2",
-        "neutral-800": "#04080f",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
